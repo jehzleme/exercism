b36ffd1177f940728233beea13f0ad2a533cfa72
Jéssica Leme Ambrust <jessica.ambrust@hbsis.com.br>
2020-08-31 13:50:51 -0300

    Hamming ok

---
+++ teams/padawan-2020-maringa/csharp/hamming/Hamming.cs
@@ -0,0 +1,24 @@
+using System;
+
+public static class Hamming
+{
+    public static int Distance(string firstStrand, string secondStrand)
+    {
+        var contador = 0;
+
+        if (firstStrand == secondStrand) return 0;
+        if (firstStrand.Length != secondStrand.Length
+            || firstStrand != null && secondStrand == null
+            || firstStrand == null && secondStrand != null)
+            throw new ArgumentException();
+        else
+        {
+            for (int i=0; i<firstStrand.Length; i++)
+            {
+                if (firstStrand[i] != secondStrand[i])
+                    contador++;
+            }
+            return contador;
+        }
+    }
+}
