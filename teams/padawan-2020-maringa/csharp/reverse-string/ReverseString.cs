43d69ec79f2bc9a333953c2fe26dd43c04ea8600
Jéssica Leme Ambrust <jessica.ambrust@hbsis.com.br>
2020-09-02 10:25:40 -0300

    ReverseString ok

---
+++ teams/padawan-2020-maringa/csharp/reverse-string/ReverseString.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public static class ReverseString
+{
+    public static string Reverse(string input)
+    {
+        char[] stringToCharArray = input.ToCharArray();
+        Array.Reverse(stringToCharArray);
+        return new string(stringToCharArray);
+    }
+}
