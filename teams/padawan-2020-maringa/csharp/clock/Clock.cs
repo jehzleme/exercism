4cc84ae16a815e61ccef4de5ec38f490c5b85466
Jéssica Leme Ambrust <jessica.ambrust@hbsis.com.br>
2020-09-08 11:16:32 -0300

    Sum of multiples ok

---
+++ teams/padawan-2020-maringa/csharp/clock/Clock.cs
@@ -0,0 +1,22 @@
+using System;
+
+public partial class Clock
+{
+    internal int Hours { get; }
+    internal int Minutes { get; }
+    public Clock(int hours, int minutes)
+    {
+        Hours = (hours + (minutes / 60)) % 24;        Minutes = minutes % 60;        if (Minutes < 0)        {            Hours--;            Minutes = 60 + Minutes;        }        if (Hours < 0)        {            Hours = 24 + Hours;        }
+
+    }
+
+    public Clock Add(int minutesToAdd)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+
+    public Clock Subtract(int minutesToSubtract)
+    {
+        throw new NotImplementedException("You need to implement this function.");
+    }
+}
--- teams/padawan-2020-maringa/csharp/sum-of-multiples/SumOfMultiples.cs
+++ teams/padawan-2020-maringa/csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> lista = new List<int>();
+        var collectionOfMultiples = multiples.Where(q => q != 0);
+
+        foreach (var item in collectionOfMultiples)
+        {
+            var i = 1;
+            while ((item * i) < max)
+            {
+                var calculo = item * i;
+                if (calculo >= max)
+                {
+                    break;
+                }
+                else
+                {
+                    lista.Where(x => x == calculo).Any();
+                    if (!lista.Where(x => x == calculo).Any())
+                    {
+                        lista.Add(calculo);
+                    }
+                }
+                i++;
+            }
+        }
+        return lista.Sum();
     }
 }
